a1113e43acf876a1aae2e6a68529ef4d7888c507
DmitriiDenisov <denisdmit03@gmail.com>
2021-09-13 18:04:30 +0300

    first

---
+++ src/Lab1.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+import static java.lang.Math.*;
+import static java.lang.System.in;
+
+public class Lab1 {
+    private final int longA = 7;
+    private final int longX = 17;
+    private final double max = -14.0;
+    private final double min = 6.0;
+    private long[] a = new long[longA];
+    private double[] x = new double[longX];
+    private double[][] b = new double[longA][longX];
+    public void update() {
+        inputArrayA();
+        System.out.println(Arrays.toString(a));
+        inputArrayX();
+        System.out.println(Arrays.toString(x));
+        inputArrayB();
+        System.out.println(Arrays.deepToString(b));
+    }
+    private void inputArrayA() {
+        int i = 0;
+        for (i = 0; i < a.length; i++){
+            a[i] = i * 2L + 6L;
+        }
+    }
+    private void inputArrayX() {
+        int i = 0;
+        for (i = 0; i < x.length; i++) {
+            x[i] = (Math.random()*((max-min)+1)) + min;
+        }
+    }
+    private void inputArrayB() {
+        int i = 0;
+        for (i = 0; i < a.length; i++) {
+            int j = 0;
+            for (j = 0; j <x.length; j++) {
+                if (a[i] == 16L) {
+                    b[i][j] = sin(cbrt(log(abs(x[j]))));
+                }
+                else if (a[i] == 6L || a[i] == 12L || a[i] == 14L) {
+                    b[i][j] = atan(pow(cos(atan(((x[j] - 4.0) / 2.0) * E + 1.0) - 1.0), 2.0));
+                }
+                else {
+                    b[i][j] = tan(pow(2.0 * 3.0 / atan(((x[j] - 4.0) / 2.0) * E + 1.0), sin(pow(x[j] / 2.0, x[j]))));
+                }
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    private static final Lab1 lab1 = new Lab1();
+    public static void main(String[] args) {
+        lab1.update();
+    }
+}
