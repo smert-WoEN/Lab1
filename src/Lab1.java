34753d51d78e96d73fdad11106fdc583e0bd018e
DmitriiDenisov <denisdmit03@gmail.com>
2021-09-18 00:04:41 +0300

    4

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,59 +1,60 @@
 import static java.lang.Math.*;
 
 public class Lab1 {
+    // Объявление и присовение значений объектам
     private final int longA = 7;
     private final int longX = 17;
     private final double max = -14.0;
     private final double min = 6.0;
     private long[] a = new long[longA];
     private double[] x = new double[longX];
-    private double[][] b = new double[longA][longX];
+    private double[][] A = new double[longA][longX];
 
-    public void update() {
+    public void update() { // Выполнение программы
         inputArrayA();
         inputArrayX();
-        inputArrayB();
+        inputArrayA1();
         outArray();
     }
 
-    private void inputArrayA() {
+    private void inputArrayA() { // Заполнение массива a (первого)
         int i = 0;
         for (i = 0; i < a.length; i++){
             a[i] = i * 2L + 6L;
         }
     }
 
-    private void inputArrayX() {
+    private void inputArrayX() {    // Заполнение массива b (второго)
         int i = 0;
         for (i = 0; i < x.length; i++) {
             x[i] = (Math.random()*((max-min)+1)) + min;
         }
     }
 
-    private void inputArrayB() {
+    private void inputArrayA1() { // Заполнение массива A (третьего)
         int i = 0;
         for (i = 0; i < a.length; i++) {
             int j = 0;
             for (j = 0; j <x.length; j++) {
                 if (a[i] == 16L) {
-                    b[i][j] = sin(cbrt(log(abs(x[j]))));
+                    A[i][j] = sin(cbrt(log(abs(x[j]))));
                 }
                 else if (a[i] == 6L || a[i] == 12L || a[i] == 14L) {
-                    b[i][j] = atan(pow(cos(atan(((x[j] - 4.0) / 2.0) * E + 1.0) - 1.0), 2.0));
+                    A[i][j] = atan(pow(cos(atan(((x[j] - 4.0) / 2.0) * E + 1.0) - 1.0), 2.0));
                 }
                 else {
-                    b[i][j] = tan(pow(2.0 * 3.0 / atan(((x[j] - 4.0) / 2.0) * E + 1.0), sin(pow(x[j] / 2.0, x[j]))));
+                    A[i][j] = tan(pow(2.0 * 3.0 / atan(((x[j] - 4.0) / 2.0) * E + 1.0), sin(pow(x[j] / 2.0, x[j]))));
                 }
             }
         }
     }
 
-    private void outArray(){
+    private void outArray(){ // Вывод данных в соотвесвии с задачей
         int i = 0;
         for (i = 0; i < longA; i++) {
             int j = 0;
             for (j = 0; j < longX; j++) {
-                System.out.printf("%12.4f", b[i][j]);
+                System.out.printf("%8.4f", A[i][j]);
             }
             System.out.println();
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    private static final Lab1 lab1 = new Lab1();
+    private static final Lab1 lab1 = new Lab1(); // Создание объекта из класса
     public static void main(String[] args) {
-        lab1.update();
+        lab1.update(); // Вызов программы
     }
 }
